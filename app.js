1647415c9d3cbc25109bef57b4beb2a8d9b42c1c
Barkamol <barkamol@gmail.com>
2021-07-17 12:43:23 +0300

    Dassrdagi dasturlar

---
+++ app.js
@@ -0,0 +1,29 @@
+// let ar = ['Html', 'css', 'JavaScript', 'Python'];
+// let num = [15, 45, 63, 0, 41, 2, 26]
+
+// var juft = 0
+// var toq = 0
+// num.forEach((value, index)=>{
+//     if (index%2===0) {
+//         juft = juft + value
+//     } else {
+//         toq = toq + value
+//     } 
+// })
+// console.log(juft); 
+
+// console.log(ar.forEach((a)=>{
+//     console.log(a);
+//     return
+// }));
+
+// ar.map((a, b, c)=> {
+//     console.log(a, b);
+// })
+
+// console.log(num.filter((value)=>value !== 45));
+// console.log(ar.sort((a,b)=> a.localeCompare(b)));
+// console.log(ar.sort());
+// const test =()=> {} 
+// console.log(num.sort((a,b)=>{return b-a}));
+// console.log(num.sort((a,b)=> a-b ));
